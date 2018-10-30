1f1cc335ee796a0045cc36b2963cdcd98ed103de
Madalina Olariu <madalinaolariu67@gmail.com>
2018-10-31 00:06:47 +0200

    repared some things

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,22 @@
 
 function hideOtherViews() {
 
-    var lista = document.querySelectorAll(".show-view");
+    var list = document.querySelectorAll(".show-view");
 
 
-    for ($i = 0; $i < lista.length; $i++) {
+    for ($i = 0; $i < list.length; $i++) {
 
-        lista[$i].classList.remove("show-view");
-        lista[$i].classList.add("hide-view");
+        list[$i].classList.remove("show-view");
+        list[$i].classList.add("hide-view");
     }
 }
 
 
 function toggleMainMenu(id) {
-
-    if (id.localeCompare('course-view') == 0) {
-
-        var r = document.getElementById('course-view');
+        var view = document.getElementById(id);
         hideOtherViews();
 
-        r.classList.remove("hide-view");
-        r.classList.add("show-view");
-
-
-    }
-    else
-        if (id.localeCompare('reports-view') == 0) {
-
-            var r = document.getElementById('reports-view');
-            hideOtherViews();
-            r.classList.remove("hide-view");
-            r.classList.add("show-view");
-        }
-        else if (id.localeCompare('attend') == 0) {
-
-            var r = document.getElementById('attend');
-            hideOtherViews();
-            r.classList.remove("hide-view");
-            r.classList.add("show-view");
-        }
-        else if (id.localeCompare('register-class-view') == 0) {
-
-            var r = document.getElementById('register-class-view');
-            hideOtherViews();
-            r.classList.remove("hide-view");
-            r.classList.add("show-view");
-        }
-        else if (id.localeCompare('statistics-view') == 0) {
-
-            var r = document.getElementById('statistics-view');
-            hideOtherViews();
-            r.classList.remove("hide-view");
-            r.classList.add("show-view");
-        }
-        else if (id.localeCompare('class-view') == 0) {
-
-            var r = document.getElementById('class-view');
-            hideOtherViews();
-            r.classList.remove("hide-view");
-            r.classList.add("show-view");
-        }
+        view.classList.remove("hide-view");
+        view.classList.add("show-view");
 }
 
