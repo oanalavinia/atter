4a446cb0148119ead882a48377ac03fe2b4789c6
Madalina Olariu <madalinaolariu67@gmail.com>
2018-12-26 17:05:28 +0200

    finised spa application

---
+++ public/js/options-professor.js
@@ -0,0 +1,11 @@
+(function () {
+    function init() {
+        var router = new Router([
+            new Route('reports-professor', 'reports-professor-view.html', true),
+            new Route('register', 'register-class-view.html'),
+            new Route('statistics', 'statistics-view.html'),
+            new Route('statistics/group-name', 'group-view.html'),
+        ]);
+    }
+    init();
+}());
